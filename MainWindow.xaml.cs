193691dcc3536272c1b097270fe3dc976ee581a9
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-05-04 11:58:17 +1000

    XAML changed and events added for clicks

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace Calculator_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum Operation { Add, Subtract, Multiply, Divide, Equals, Start };
+        Operation currentOperation = Operation.Start;
+        double currentValue = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            txtResult.Text = currentValue.ToString();
         }
+
+        //Number button clicked
+        private void btnNumber_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Multiplication button clicked
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        //Division button clicked
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        //Subtraction button clicked
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        //Addition button clicked
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        //Equal to button clicked
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Clear the current results
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
     }
 }
